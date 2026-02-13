86094c19107146ea9f932ccaf45f1c4dbb1ab190
Aditya <adi247u@gmail.com>
2026-02-13 15:57:42 +1100

    chore: configure PostgreSQL and Redis connections

--- src/main/java/com/ad/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/ad/api/gateway/ApiGatewayApplication.java
@@ -3,10 +3,18 @@ package com.ad.api.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load(); // Loads .env file
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
+		System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
